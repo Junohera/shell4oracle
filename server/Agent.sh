d6fce94a1f30ad92a64f63c3b659222c6b6da261
juno <selectjuno@gmail.com>
2023-08-24 10:14:52 +0900

    agent

---
+++ server/Agent.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 0. export project root path
+# 1. set environment
+# 2. check shell file
+###########################################
+# 0. export project root path
+export_project_root_path () {
+  local full_path
+  local file
+  local name
+
+  full_path=$0
+  file=$(echo $full_path | awk -F"/" '{print $NF}')
+  name=$(echo $file | awk -F"." '{print $1}')
+
+  PROJECT_ROOT=$(echo $full_path | awk -F"$file" '{print $1}')
+
+  # echo "full_path : ${full_path}"; echo "file : ${file}"; echo "name : ${name}"; echo "PROJECT_ROOT : ${PROJECT_ROOT}";
+  export PROJECT_ROOT  
+}
+# 1. set environment
+set_environment () {
+  cd $PROJECT_ROOT || exit
+  echo $(cat .color.env)
+}
+# 2. check shell file
+check_shell_file() {
+  if [ $# -lt 1 ]; then
+    echo "No shell file was specified to run. (example: sh Agent.sh 'sample')"
+    exit
+  fi
+}
+###########################################
+export_project_root_path
+set_environment
+check_shell_file "$@"
+
---
+++ server/sample/sample.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+OUTPUT="$(date +%F)"
+echo "$OUTPUT"
